baca7f68d5c68a55a3e9619488684e6b5294a096
WataruMaeda <w.maeda.jp@gmail.com>
2021-07-20 01:45:18 -0700

    chore: configure env variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,18 +4,9 @@ import 'firebase/firestore'
 import 'firebase/storage'
 import 'firebase/performance'
 import 'firebase/analytics'
+import config from './config'
 
-const config = {
-  apiKey: 'AIzaSyBBNLnLuFLMkRuiVGmqRTKRXkgEYPj2un4',
-  authDomain: 'react-firebase-boilerpla-ce757.firebaseapp.com',
-  databaseURL: 'https://react-firebase-boilerpla-ce757.firebaseio.com',
-  projectId: 'react-firebase-boilerpla-ce757',
-  storageBucket: 'react-firebase-boilerpla-ce757.appspot.com',
-  messagingSenderId: '698838279601',
-  appId: '1:698838279601:web:9a2229262fea8f9a490990',
-}
-
-firebase.initializeApp(config)
+firebase.initializeApp(config.firebase)
 firebase.analytics()
 
 export const auth = firebase.auth()
