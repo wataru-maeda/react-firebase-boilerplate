3643af1ca58dda6b987b835dab3e9e9c81e5180d
WataruMaeda <w.maeda.jp@gmail.com>
2021-12-25 15:04:52 -0800

    chore: upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,34 @@
+// module.exports = {
+//   "stories": [
+//     "../src/**/*.stories.mdx",
+//     "../src/**/*.stories.@(js|jsx|ts|tsx)"
+//   ],
+//   "addons": [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/preset-create-react-app"
+//   ],
+//   "framework": "@storybook/react"
+// }
+
 const path = require('path')
 
 module.exports = {
   stories: [
     '../src/__stories__/*.stories.mdx',
-    '../src/__stories__/*.stories.@(js|jsx|ts|tsx)',
+    '../src/__stories__/*.stories.js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
   ],
-  webpackFinal: async (config) => {
-    config.resolve.modules = [
-      ...(config.resolve.modules || []),
-      path.resolve('../src'),
-    ]
-    return config
-  },
+  framework: '@storybook/react',
+  // webpackFinal: async (config) => {
+  //   config.resolve.modules = [
+  //     ...(config.resolve.modules || []),
+  //     path.resolve('../src'),
+  //   ]
+  //   return config
+  // },
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from 'app'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './app.scss'
+import 'theme/app.scss'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
